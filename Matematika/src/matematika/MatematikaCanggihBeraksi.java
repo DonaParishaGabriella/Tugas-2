fe79e6ba42a38198ecd23f253dc197779fabf1df
DonaParishaGabriella <donafarishag@gmail.com>
2024-12-04 14:38:15 +0700

    Update MatematikaCanggihBeraksi.java

--- Matematika/src/matematika/MatematikaCanggihBeraksi.java
+++ Matematika/src/matematika/MatematikaCanggihBeraksi.java
@@ -11,25 +11,29 @@ package matematika;
 public class MatematikaCanggihBeraksi {
     public static void main(String[] agrs){
         MatematikaCanggih mc = new MatematikaCanggih();
-        mc.pertambahan(34,13);
-        mc.pertambahan(3.4,1.3);
-        mc.pertambahan(34,13,22);
-        mc.pertambahan(3.4,1.3,2.2);
-        mc.pengurangan(12, 3);
-        mc.pengurangan(1.2, 3.0);
-        mc.pengurangan(12, 3, 6);
-        mc.pengurangan(1.2, 3.0, 6.0);
-        mc.perkalian(34, 45);
-        mc.perkalian(3.4, 4.5);
-        mc.perkalian(34, 45, 32);
+        mc.pertambahan(32,18);
+        mc.pertambahan(3.1,1.4);
+        mc.pertambahan(23,16,22);
+        mc.pertambahan(2.4,1.8,2.2);
+        
+        mc.pengurangan(11, 9);
+        mc.pengurangan(1.5, 3.0);
+        mc.pengurangan(8, 7, 4);
+        mc.pengurangan(1.8, 4.0, 6.0);
+        
+        mc.perkalian(24, 44);
+        mc.perkalian(5.4, 4.5);
+        mc.perkalian(24, 55, 22);
         mc.perkalian(3.4, 4.5, 3.2);
+        
         mc.pembagian(67, 34);
         mc.pembagian(6.7, 3.4);
         mc.pembagian(67, 34, 44);
         mc.pembagian(6.7, 3.4, 4.4);
-        mc.modulus(10, 3);
-        mc.modulus(2.1, 1.0);
-        mc.modulus(2, 10, 14);
-        mc.modulus(2.1, 1.0, 1.4);    
+        
+        mc.modulus(18, 3);
+        mc.modulus(2.3, 1.0);
+        mc.modulus(8, 10, 14);
+        mc.modulus(2.2, 1.9, 1.2);    
     }
 }
